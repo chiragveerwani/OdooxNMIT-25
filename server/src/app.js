5d5d4f28d9b76b097fe5de3eaa78aa8086cf3da3
chiragveerwani <chiraguv2006@gmail.com>
2025-09-06 09:34:03 +0530

    building the foundation

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,24 @@
-// Express server entry point
 const express = require('express');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
-  res.send('Welcome to OdooxNMIT-25 Server');
+  res.json({ 
+    message: 'SynergySphere API is running!',
+    version: '1.0.0'
+  });
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
